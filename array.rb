d2e4b382b52774f6ec5e2b2d165e8ca82bcadc9c
consultorsandro <consultorsandro@hotmail.com>
2022-11-14 16:35:20 -0300

    visão extremamente básica de array

---
+++ array.rb
@@ -0,0 +1,22 @@
+# semelhante a matriz ou vetores em C++
+# pode incluir tipos diferentes no array
+
+lista = [1,2,3, "oi"]
+lista.class 
+p lista.class  # exibe o tipo da lista
+
+p lista.length # exibe quantidade de elementos e posições no array
+
+p lista # exibe a lista, inclusive com tipos diferentes
+
+p lista.empty? # verfica se está vazia (bool), se não há parâmetros, a função pode omitir parênteses
+p lista.empty?() # verfica se está vazia (bool)
+
+lista1 = [1,2]
+lista2 = [2,3]
+
+listas_juntas = lista1 + lista2 # Arrays podem ser unidos, conforme ordem apresentada
+listas_juntas = lista2 + lista1  
+p listas_juntas
+
+
