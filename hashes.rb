46e57a44173bdcaefb29333524f117333f841a29
consultorsandro <consultorsandro@hotmail.com>
2022-11-15 16:43:12 -0300

    "Basico de Hashes"

---
+++ hashes.rb
@@ -0,0 +1,17 @@
+# lista formada pela combinação chave/valor
+
+h = {} # inicia um novo hashe
+p  h.keys # exibe as chaves do hashe
+
+h[:nome]= "Sandro"
+h[:idade]= 49
+p h[:nome]
+p h[:idade]
+
+h = {:nome=> "Sandro", :idade=>39, :Ano_da_liberdade=>50}
+
+p h[:nome]
+p h[:idade]
+p  h.keys 
+
+p h.values
